c59adb173432d90da77f48f3481b21ea035526d6
hkaur008 <hargunalone@gmail.com>
2021-06-19 19:34:20 +0530

    basic app created

---
+++ readme.md
@@ -0,0 +1,14 @@
+This webrtc based video streaming feature !
+
+1. Intialise nodejs project ✅
+2. Initialise view ✅
+3. Create a random string to enter a room ✅
+4. Ability to view one person video ✅
+5. Add other people in same stream 
+6. Add styling
+7. Create messages
+8. Mute button
+9. Stop video button 
+
+Express is used to build API .
+uuid will generate unique id .
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const path = require('path'); 
+const { v4: uuidv4 } = require('uuid');
+const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3030;
+// Peer
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use('/peerjs', peerServer);
+
+app.get( '/', (req, res) => {
+  res.send(`main`);
+});
+
+app.get( '/teams-webrtc', (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get( '/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId ,userName) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit('user-connected', userId);
+
+    socket.on('message', (message ) => {
+      io.to(roomId).emit('createMessage', message , userName);
+    });
+  });
+});
+
+server.listen( PORT , () => console.log(`Server listening to port ${PORT}`));
