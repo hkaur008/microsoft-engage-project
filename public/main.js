4ffa9b24139076a2bd8e1be74a5b9fc78dc88480
hkaur008 <hargunalone@gmail.com>
2021-07-10 02:47:00 +0530

    heroku branch

--- public/main.js
+++ public/main.js
@@ -13,7 +13,8 @@ const join_meet = document.getElementById("join-meet");
 
 const state = "out-meet";
 const peers = {}
-let myName ;
+let myName = prompt('Please enter your name', 'Hargun');
+;
 // messenger
 let messageStatus = 0;
 const msgerForm = get(".msger-inputarea");
@@ -93,7 +94,6 @@ peer.on('open', (id) => {
 console.log(myName + " first");
 //username input
 const nameInput = (id)=> {
-   myName = prompt('Please enter your name', 'Hargun');
   if (myName != null) {
      
      messagesRef.on('value', (snapshot) => {
