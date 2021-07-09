704addf1b8d8ce805b86c5cd8826f5ea8b86392d
hkaur008 <hargunalone@gmail.com>
2021-07-09 15:03:20 +0530

    heroku app

--- public/main.js
+++ public/main.js
@@ -32,7 +32,7 @@ let myId = null ;
 var peer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
-  port: '3000',
+  port: '443',
 });
 
 
--- public/script.js
+++ public/script.js
@@ -45,7 +45,7 @@ let myId = null ;
 var peer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
-  port: '3000',
+  port: '443',
 });
 
 let myVideoStream;
