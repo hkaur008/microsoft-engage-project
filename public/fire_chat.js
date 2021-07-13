7563b45899b418e9c1177a0aa3e3d886eef14b24
hkaur008 <hargunalone@gmail.com>
2021-07-13 11:46:26 +0530

    enviroment variable set up

--- public/fire_chat.js
+++ public/fire_chat.js
@@ -1,14 +1,6 @@
 
   // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBa4MFefKdG4AUn2bnOwsXVg9xO_DKLKEw",
-    authDomain: "engage-project-9d8d3.firebaseapp.com",
-    databaseURL: "https://engage-project-9d8d3-default-rtdb.firebaseio.com",
-    projectId: "engage-project-9d8d3",
-    storageBucket: "engage-project-9d8d3.appspot.com",
-    messagingSenderId: "713389628767",
-    appId: "1:713389628767:web:fcbc43b782dca70e3eaaea"
-  };
+  var firebaseConfig = 'KEYS_TO_BE_HERE'
 
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
